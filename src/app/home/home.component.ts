0d46a73f3b293cdb8be94ca133851eb0528ca06c
EsraaTarek382 <esraatareksamir@gmail.com>
2025-07-18 23:41:56 +0300

    error handling messages

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
+import { MessagesService } from '../messages/messages.service';
 // Before the component was using http service to pull the component inside the view
 //Now it only knows about the coursesService which is an abstraction of the http service
 
@@ -27,7 +28,9 @@ export class HomeComponent implements OnInit {
 
 
   constructor(private courseService:CoursesService,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private messagesService: MessagesService,
+   
   ) {
 
   }
@@ -46,8 +49,11 @@ reloadCourses(){
 //shareReplay to solve this issue
 const courses$ = this.courseService.loadCourses().pipe(
 map(courses => courses.sort(sortCoursesBySeqNo)),
-finalize(() => {
-  this.loadingService.loadingOff();
+catchError(err => {
+ const errorMessage = 'Could not load courses. Please try again later.';
+ this.messagesService.showErrors(errorMessage);
+  console.error(errorMessage, err);
+  return throwError(err);
 })
 )
 const loadCourses$=this.loadingService.showLoadingUntilComplete(courses$)
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs';
 import {Message} from '../model/message';
 import {tap} from 'rxjs/operators';
+import { MessagesService } from './messages.service';
 
 @Component({
     selector: 'messages',
@@ -10,20 +11,26 @@ import {tap} from 'rxjs/operators';
     standalone: false
 })
 export class MessagesComponent implements OnInit {
+ 
+  showmessages=false; 
 
+  errors$: Observable<string[]>;
 
-  constructor() {
-
+  constructor(public messagesService: MessagesService) {
+    // You can inject the MessagesService here if needed
+    console.log('MessagesComponent constructor called');
   }
 
   ngOnInit() {
-
+this.errors$ = this.messagesService.errors$.pipe(
+  tap(()=> this.showmessages = true) // Show messages when errors are emitted
+)
 
   }
 
 
   onClose() {
-
+this.showmessages=false;
 
   }
 
